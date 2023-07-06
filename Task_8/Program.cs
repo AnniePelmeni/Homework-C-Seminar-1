130acd06abdc88a9921b56e959a5deef76e2904f
Anna <vitani2007@mail.ru>
2023-07-06 10:17:39 +0300

    Task 8 solved

---
+++ Task_8/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число: ");
+int number1 = int.Parse (Console.ReadLine());
+
+if (number1 >= 0)
+{
+    int number2 = 2;
+    while (number2 <= number1)
+    {
+    Console.Write(number2 + " ");
+    number2 = number2 + 2;
+    }
+}
+
+else
+{
+    int number3 = 0;
+    while (number3 >= number1)
+    {
+    Console.Write(number3 + " ");
+    number3 = number3 - 2;
+    }
+}
