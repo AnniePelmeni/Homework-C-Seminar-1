73e22d6122263af13c556a494759137eb482b2ae
Anna <vitani2007@mail.ru>
2023-07-06 09:52:41 +0300

    Task 2 solved

---
+++ Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Число {number1} больше числа {number2}");
+}
+
+if (number1 < number2)
+{
+    Console.WriteLine($"Число {number1} меньше числа {number2}");
+}
+
+if (number1 == number2)
+{
+    Console.WriteLine($"Число {number1} равно числу {number2}");
+}
